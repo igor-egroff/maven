5bed7a41fe998f22054643d2a21e6a83cb5f29e0
igor-eg <igor73@list.ru>
2022-01-01 13:28:17 +0300

    Первоначальная версия

---
+++ src/main/java/db/Db.java
@@ -0,0 +1,29 @@
+package db;
+
+import java.util.UUID;
+
+import static java.util.UUID.*;
+
+public class Db {
+
+    private DbSetting dbSetting;
+    private MyEntity myEntity;
+
+    public Db(DbSetting dbSetting) {
+        this.dbSetting = dbSetting;
+        myEntity = new MyEntity("first");
+        myEntity.setId(randomUUID());
+    }
+
+    public DbSetting getDbSetting() {
+        return dbSetting;
+    }
+
+    public MyEntity getMyEntity() {
+        return myEntity;
+    }
+
+    public void setMyEntity(MyEntity myEntity) {
+        this.myEntity = myEntity;
+    }
+}
